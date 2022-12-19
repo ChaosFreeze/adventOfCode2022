2485f7fd6344288ec4f7db662ca9922098390612
ChaosFreeze <subinmohan77@gmail.com>
2022-12-19 18:49:08 +0530

    modified day1.py

--- day1.py
+++ day1.py
@@ -3,9 +3,7 @@
 with open('inputs/day1.in') as input_file:
     elf_calories = [[int(calory) for calory in calories.split('\n')] for calories in input_file.read().split('\n\n')]
 
-sum_of_calories = [sum(i) for i in elf_calories]
+sum_of_calories: list[int] = [sum(i) for i in elf_calories]
 max_calory = max(sum_of_calories)
 elf_with_max_calory = sum_of_calories.index(max_calory)
-print(f"The elf carrying most calories is elf no {elf_with_max_calory} which is carrying {max_calory} calories.")
-sum_of_three_elves = sum(sorted(sum_of_calories, reverse=True)[:3])
-print(sum_of_three_elves)
+print(elf_with_max_calory, max_calory)
