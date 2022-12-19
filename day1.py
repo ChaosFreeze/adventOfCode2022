43f525f27a1e5fc77cf3427084314d5ef2ab4098
ChaosFreeze <subinmohan77@gmail.com>
2022-12-19 18:19:42 +0530

    added day1 solution

---
+++ day1.py
@@ -0,0 +1,9 @@
+# Calorie Counting
+
+with open('inputs/day1.in') as input_file:
+    elf_calories = [[int(calory) for calory in calories.split('\n')] for calories in input_file.read().split('\n\n')]
+
+sum_of_calories = [sum(i) for i in elf_calories]
+max_calory = max(sum_of_calories)
+elf_with_max_calory = sum_of_calories.index(max_calory)
+print(elf_with_max_calory)
